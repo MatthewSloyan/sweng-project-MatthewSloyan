e1435fa7801439e1c5a01ccf59211bbd3f9953b2
Matthew Sloyan <g00348036@gmit.ie>
2020-05-04 11:58:44 +0000

    Added Rspec tests for deleting recipe, will now add delete functionality.

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -3,16 +3,17 @@ require 'rails_helper'
 # Tests below coded and adapted from lecture videos supplied.
 describe RecipesController, type: 'controller' do
 
+  # == SETUP ==
+  # Helper method to create a new recipe, can be used in multiple tests.
+  let(:create_recipe) { 
+      post :create, recipe: { recipe_name: 'Lasagne', description: 'Sample Description', difficulty: 'Medium', servings: 4, 
+        cook_time: '20 Minutes', steps: ['Step 1.', 'Step 2.'], ingredients: ['Ingredient 1', 'Ingredient 2'], author: 'Test User'
+    }
+  }
+
   # == CREATE == 
   describe "#create" do
     context "When a user adds a new recipe" do
-
-      # Helper method to create a new movie, can be used in each test.
-      let(:create_recipe) { 
-          post :create, recipe: { recipe_name: 'Lasagne', description: 'Sample Description', difficulty: 'Medium', servings: 4, 
-            cook_time: '20 Minutes', steps: ['Step 1.', 'Step 2.'], ingredients: ['Ingredient 1', 'Ingredient 2'], author: 'Test User'
-          }
-      }
 
       it "should create a new recipe" do
         # Expect post request created to change recipe count by one (add recipe)
@@ -34,18 +35,47 @@ describe RecipesController, type: 'controller' do
     end
   end
 
+  # == DESTROY/DELETE == 
+  describe "#delete" do
+    context "When a user deletes a recipe" do
+
+      it "should destroy a given recipe" do
+        # Create post request (Helper method).
+        create_recipe
+
+        # Expect delete request created to change movie count by zero as movie is created and deleted (delete movie)
+        expect { delete :destroy, :id => '1' }.to change { Recipe.count }.by(0)
+      end
+
+      it "should return to home page and display success message" do
+        # Create post request (Helper method).
+        create_recipe
+
+        # Delete new recipe created above, by calling destroy method.
+        delete :destroy, :id => '1'
+
+        # Expect to reload home page.
+        expect(@respose).to redirect_to(:recipes)
+
+        # Expect to display flash message for movie.
+        # Code adated from: https://stackoverflow.com/questions/24919976/rspec-3-how-to-test-flash-messages
+        expect(flash[:notice]).to eq("Lasagne was successfully deleted.")
+      end
+    end
+  end
+
   # == SHOW == 
   describe "#show" do
     context "When a user selects a recipe" do
       it "should show the recipe information" do
-        # Setup fake results.
+        # Setup fake results, can be used in multiple tests.
         @fake_results = double('Recipe', recipe_name: 'Lasagne', description: 'Sample Description', difficulty: 'Medium', servings: 4, 
-            cook_time: '20 Minutes', steps: ['Step 1.', 'Step 2.'], ingredients: ['Ingredient 1', 'Ingredient 2'], author: 'Test User')
-    
-        # Have to add stub for :find or else it would throw error that id not found in movies_controller.
+          cook_time: '20 Minutes', steps: ['Step 1.', 'Step 2.'], ingredients: ['Ingredient 1', 'Ingredient 2'], author: 'Test User')
+
+        # Have to add stub for :find or else it would throw error that id not found in recipes_controller.
         allow(Recipe).to receive(:find).and_return(@fake_results)
 
-        # Call show method, and expect found recipe to equal @fake_results
+        # Call show method, and expect found recipe to equal @fake_results and for template to render.
         get :show, {:id => "1"}
         expect(Recipe.find("1")).to eq(@fake_results)
         expect(@respose).to render_template('recipes/show')
